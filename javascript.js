eb9e3b40452a3b6c877ef48e6077ab64e894309c
Sonam Rani <sonamrani11092004@gmail.com>
2023-08-03 05:53:08 +0530

    second commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let slideBtnleft= document.getElementById("slide-btn-right")
+let slideBtnRight= document.getElementById("slide-btn-right")
+
+slideBtnleft.addEventListener("click",()=>{
+    alert("left-btn")
+})
+slideBtnRight.addEventListener("click",()=>{
+    alert("Right-btn")
+})
+
